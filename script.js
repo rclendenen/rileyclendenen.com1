d2fab09fce181be52e48d7e52281a2ff875ee320
Elizabeth K. Green <writeovercoffeee@gmail.com>
2025-09-18 22:30:18 -0500

    Remove white heart emoji from notification message to fix display issue

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ if (contactForm) {
                 submitBtn.disabled = false;
                 
                 // Show notification
-                showNotification('Message sent successfully! I\'ll get back to you soon. 🤍', 'success');
+                showNotification('Message sent successfully! I\'ll get back to you soon.', 'success');
                 
                 // Reset form
                 contactForm.reset();
